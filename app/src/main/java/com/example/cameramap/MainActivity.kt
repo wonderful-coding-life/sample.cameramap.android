3099fd7fb4a8db15e85f3591aa9cb0de92196c23
Heesun Kim <spiderman@xhift.co.kr>
2023-09-14 11:43:32 +0900

    added camping list

--- app/src/main/java/com/example/cameramap/MainActivity.kt
+++ app/src/main/java/com/example/cameramap/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.cameramap
 
 import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,16 +8,13 @@ import android.util.Log
 import com.example.cameramap.databinding.ActivityMainBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.TileOverlayOptions
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.heatmaps.Gradient
 import com.google.maps.android.heatmaps.HeatmapTileProvider
-import com.google.maps.android.heatmaps.WeightedLatLng
 
 private const val TAG = "MainActivity"
 
@@ -32,51 +28,28 @@ class MainActivity : AppCompatActivity() {
         mapFragment?.getMapAsync {googleMap ->
 
             // set initial position and zoom
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(37.56682420, 126.97865226), 16F))
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(37.56682420, 126.97865226), 8F))
 
             // set minimum zoom level
-            googleMap.setMinZoomPreference(4F)
+            //googleMap.setMinZoomPreference(4F)
 
             // get camara map from json file
-            val jsonString = this@MainActivity.assets.open("camera.json").bufferedReader().readText()
-            val jsonType = object : TypeToken<CameraMap>() {}.type
-            val cameraMap = Gson().fromJson(jsonString, jsonType) as CameraMap
+            val jsonString = this@MainActivity.assets.open("camping.json").bufferedReader().readText()
+            val jsonType = object : TypeToken<CampingMap>() {}.type
+            val campingMap = Gson().fromJson(jsonString, jsonType) as CampingMap
 
             // create cluster and add camera
-            val clusterManager = ClusterManager<Camera>(this@MainActivity, googleMap)
-            googleMap.setOnCameraIdleListener(clusterManager)
-            googleMap.setOnMarkerClickListener(clusterManager)
-            cameraMap.camera.forEach { clusterManager.addItem(it) }
+//            val clusterManager = ClusterManager<Camping>(this@MainActivity, googleMap)
+//            googleMap.setOnCameraIdleListener(clusterManager)
+//            googleMap.setOnMarkerClickListener(clusterManager)
+//            campingMap.camping.forEach { clusterManager.addItem(it) }
 
-            // start camera detail activity when user click item's info windows
-            clusterManager.setOnClusterItemInfoWindowClickListener { camera ->
-                val intent = Intent(this@MainActivity, CameraActivity::class.java).apply {
-                    putExtra("owner", camera.owner)
-                    putExtra("address", camera.address)
-                    putExtra("old", camera.old)
-                    putExtra("type", camera.type)
-                    putExtra("count", camera.count)
-                    putExtra("resolution", camera.resolution)
-                    putExtra("angle", camera.angle)
-                    putExtra("archive", camera.archive)
-                    putExtra("installed", camera.installed)
-                    putExtra("phone", camera.phone)
-                    putExtra("latitude", camera.latitude)
-                    putExtra("longitude", camera.longitude)
-                }
-                this@MainActivity.startActivity(intent)
-            }
-
-//            val cameraLatLngList  = ArrayList<LatLng>()
-//            cameraMap.camera.forEach { if (it.type == "교통단속") cameraLatLngList.add(it.position) }
-//            val colors = intArrayOf(
-//                Color.rgb(102, 225, 0),  // green
-//                Color.rgb(255, 0, 0) // red
-//            )
-//            val startPoints = floatArrayOf(0.2f, 1f)
-//            val gradient = Gradient(colors, startPoints)
-//            val provider = HeatmapTileProvider.Builder().data(cameraLatLngList).gradient(gradient).build()
-//            googleMap.addTileOverlay(TileOverlayOptions().tileProvider(provider))
+            // create heat map
+            val campingLatLngList  = ArrayList<LatLng>()
+            campingMap.camping.forEach { campingLatLngList.add(it.position) }
+            Log.i(TAG, "spiderman count = ${campingMap.camping.size}")
+            val provider = HeatmapTileProvider.Builder().data(campingLatLngList).build()
+            googleMap.addTileOverlay(TileOverlayOptions().tileProvider(provider))
         }
     }
 }
